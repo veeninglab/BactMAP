afbddd3be2559ec76519cbfafb7e7dee01b0eab6
adminuser <adminuser@PC-114-178.OASIS.UCLOUVAIN.BE>
2020-08-07 13:07:18 +0200

    merge merge

--- R/zzz.R
+++ R/zzz.R
@@ -2,6 +2,10 @@
 
 
 .onAttach <- function(libname,pkgname){
+<<<<<<< HEAD
   packageStartupMessage("This is the development version of BactMAP updated on 8/7/2020. Use this version for the latest bug-fixes and new functions.
                         To download the stable version of bactmap, use 'remotes::install_github('veeninglab/bactmap')'")
+=======
+  packageStartupMessage("Welcome to BactMAP version 0.1.0.2 (January 24 2020). \n see what's new, type '?bactmapnews' in your console.")
+>>>>>>> 27a9040... information added
 }
